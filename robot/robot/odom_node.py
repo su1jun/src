798eabaf2994ffc72bc503b01c73042c7b60641b
su1jun <qaxsd530@gmail.com>
2024-03-20 15:48:14 +0900

    1

--- robot/robot/odom_node.py
+++ robot/robot/odom_node.py
@@ -1,37 +1,43 @@
 import rclpy
 from rclpy.node import Node
-from tf2_ros.static_transform_broadcaster import StaticTransformBroadcaster
-from geometry_msgs.msg import TransformStamped
-import tf_transformations
+from geometry_msgs.msg import Twist, TransformStamped
+from nav_msgs.msg import Odometry
+import tf2_ros
 
-class StaticTransformPublisher(Node):
+class OdometryPublisher(Node):
     def __init__(self):
-        super().__init__('static_transform_publisher')
-        self.broadcaster = StaticTransformBroadcaster(self)
-        self.timer = self.create_timer(0.1, self.publish_transform)
+        super().__init__('odometry_publisher')
+        self.odom_pub = self.create_publisher(Odometry, 'odom', 10)
+        self.cmd_vel_sub = self.create_subscription(
+            Twist,
+            'cmd_vel',
+            self.cmd_vel_callback,
+            10)
+        self.odom_broadcaster = tf2_ros.TransformBroadcaster(self, qos=rclpy.qos.qos_profile_sensor_data)
+        self.odom = Odometry()
+        self.odom.header.frame_id = 'odom'
+        self.odom.child_frame_id = 'base_link'
 
-    def publish_transform(self):
+    def cmd_vel_callback(self, msg):
+        # just cmd_vel
+        self.odom.twist.twist = msg
+        self.odom.header.stamp = self.get_clock().now().to_msg()
+        self.odom_pub.publish(self.odom)
+
+        # TF transform
         t = TransformStamped()
-
-        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.stamp = self.odom.header.stamp
         t.header.frame_id = 'odom'
         t.child_frame_id = 'base_link'
-        
-        t.transform.translation.x = 0.0
-        t.transform.translation.y = 0.0
-        t.transform.translation.z = 0.0
-        
-        q = tf_transformations.quaternion_from_euler(0, 0, 0)
-        t.transform.rotation.x = q[0]
-        t.transform.rotation.y = q[1]
-        t.transform.rotation.z = q[2]
-        t.transform.rotation.w = q[3]
-
-        self.broadcaster.sendTransform(t)
+        t.transform.translation.x = self.odom.pose.pose.position.x
+        t.transform.translation.y = self.odom.pose.pose.position.y
+        t.transform.translation.z = self.odom.pose.pose.position.z
+        t.transform.rotation = self.odom.pose.pose.orientation
+        self.odom_broadcaster.sendTransform(t)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = StaticTransformPublisher()
+    node = OdometryPublisher()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
